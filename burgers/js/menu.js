3591a8f7f48ace29c4b6778e9f9d0bf3086eb9ba
Tatyana Gurtovenko <t.gurtovenko.sas@gmail.com>
2019-05-30 19:23:22 +0500

    Исправление

--- burgers/js/menu.js
+++ burgers/js/menu.js
@@ -1,18 +1,17 @@
 const menu__list = document.querySelector('.menu__list');
-const menu__subtitle = document.getElementsByClassName('menu__subtitle');
-const menu__item = document.getElementsByClassName('menu__item-block');
+const menu__item = document.getElementsByClassName('menu__item');
+const menu__block = document.getElementsByClassName('menu__item-block');
 
 
 
 menu__list.addEventListener('click', function(e) {
-
-    if (e.target.classList.contains('menu__subtitle--active')) {
-        e.target.classList.remove('menu__subtitle--active');
-    } else if (e.target.classList.contains('menu__subtitle')) {
-        for (let i = 0; i < menu__subtitle.length; i++) {
-            menu__subtitle[i].classList.remove('menu__subtitle--active');
+    let selectedSection = e.target.closest('.menu__item');
+    if (selectedSection.classList.contains('menu__item--active')) {
+        selectedSection.classList.remove('menu__item--active')
+    } else if (selectedSection.classList.contains('menu__item')) {
+        for (let i = 0; i < menu__item.length; i++) {
+            menu__item[i].classList.remove('menu__item--active');
         }
-        e.target.classList.toggle('menu__subtitle--active');
+        selectedSection.classList.toggle('menu__item--active');
     }
-
-});
+})
